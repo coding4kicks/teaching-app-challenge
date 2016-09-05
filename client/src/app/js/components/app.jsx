9f33b2e6edd205d72ae5239aa1c627addb3a4ec5
Poof Young <poof@coding4kicks.com>
2016-09-04 21:01:00 -0700

    Add material ui app bar
    Rename appbar to appbarcontainer so no conflict with material ui
    component.

--- client/src/app/js/components/app.jsx
+++ client/src/app/js/components/app.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Component } from 'react';
 
 import Error from './shared/error';
-import Appbar from '../containers/appbar.jsx'
+import AppBarContainer from '../containers/appbar-container'
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <Appbar />
+        <AppBarContainer />
         <Error />
         {this.props.children}
       </div>
